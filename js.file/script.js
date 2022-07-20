be19956759aa40fc0be042486c0afcfbc67d0995
tanvir477 <tanvirhassan477@gmail.com>
2022-07-20 13:08:54 +0600

    almost finished

--- js.file/script.js
+++ js.file/script.js
@@ -1,6 +1,6 @@
 //====================== Typing animation ============
 var typed = new Typed('.typing',{
-    strings:["","Web Designer", "web Developer", "Graphic Designer", "Youtuber" ],
+    strings:["","Web Designer", "Web Developer", "Graphic Designer" ],
     typeSpeed:100,
     BackSpeed:60,
     loop: true
